1d1f56a5c9561258631005e49021ea98160c0bd9
Mayara Almeida <jennim.almeida@gmail.com>
2025-04-04 17:27:00 -0300

    Início do menu lateral

---
+++ frontend/src/components/menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+    return(
+        <div class="menu-lateral">
+            <hr />
+            <a class="botao" href="/">Início</a>
+            <a class="botao" href="/">Odonto Banco</a>
+            <a class="botao" href="/">Ajustes</a>
+        </div>
+    )
+}
+
+export default Menu;
